19a417fff1ba54ec8e9d964b137234dce526b88d
Servyn <cuongthientai13@gmail.com>
2025-02-10 07:19:06 +0700

    viết các chức năng cơ bản trong Task Manager

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try(Connection conn = DatabaseConnector.getConnection()) {
+            System.out.println("Kết nối thành công");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        TaskManager tm = new TaskManager();
+        tm.completedTask(1);
+        tm.getAllTasks();
     }
+
 }
